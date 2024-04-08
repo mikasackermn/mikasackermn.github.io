8cba18580677608bc60c9681f6f5337a284db9b3
mikasackermn <mikasa@rango.exchange>
2024-04-08 16:36:21 +0330

    Improving the code according to our own needs

--- scripts/tints-and-shades.js
+++ scripts/tints-and-shades.js
@@ -1,6 +1,6 @@
 // e.g., whether the user wants copying to use a hashtag or not
 var settings = {
-  copyWithHashtag: false
+  copyWithHashtag: false,
 };
 
 // parse an input string, looking for any number of hexadecimal color
@@ -9,19 +9,17 @@ var settings = {
 function parseColorValues(colorValues) {
   var colorValuesArray = colorValues.match(/\b[0-9A-Fa-f]{3}\b|[0-9A-Fa-f]{6}\b/g);
   if (colorValuesArray) {
-    colorValuesArray = colorValuesArray.map(
-      function (item) {
-        if (item.length === 3) {
-          var newItem = item.toString().split('');
-          newItem = newItem.reduce(function (acc, it) {
-            return acc + it + it;
-          }, '');
-          return newItem;
-        }
-
-        return item;
+    colorValuesArray = colorValuesArray.map(function (item) {
+      if (item.length === 3) {
+        var newItem = item.toString().split("");
+        newItem = newItem.reduce(function (acc, it) {
+          return acc + it + it;
+        }, "");
+        return newItem;
       }
-    );
+
+      return item;
+    });
   }
 
   return colorValuesArray; // this could be null if there are no matches
@@ -29,9 +27,9 @@ function parseColorValues(colorValues) {
 
 // pad a hexadecimal string with zeros if it needs it
 function pad(number, length) {
-  var str = '' + number;
+  var str = "" + number;
   while (str.length < length) {
-    str = '0' + str;
+    str = "0" + str;
   }
   return str;
 }
@@ -42,8 +40,8 @@ function hexToRGB(colorValue) {
   return {
     red: parseInt(colorValue.substr(0, 2), 16),
     green: parseInt(colorValue.substr(2, 2), 16),
-    blue: parseInt(colorValue.substr(4, 2), 16)
-  }
+    blue: parseInt(colorValue.substr(4, 2), 16),
+  };
 }
 
 // convert an integer to a 2-char hex string
@@ -61,45 +59,49 @@ function rgbToHex(rgb) {
 
 // shade one of our rgb color objects to a distance of i*10%
 // ({ red: 80, green: 18, blue: 20 }, 1) => { red: 72, green: 16, blue: 18 }
-function rgbShade(rgb, i) {
+function rgbShade(rgb, i, mode) {
+  var percent = mode === "normal" ? 0.2 : 0.1;
+
   return {
-    red: rgb.red * (1 - 0.1 * i),
-    green: rgb.green * (1 - 0.1 * i),
-    blue: rgb.blue * (1 - 0.1 * i)
-  }
+    red: rgb.red * (1 - percent * i),
+    green: rgb.green * (1 - percent * i),
+    blue: rgb.blue * (1 - percent * i),
+  };
 }
 
 // tint one of our rgb color objects to a distance of i*10%
 // ({ red: 80, green: 18, blue: 20 }, 1) => { red: 98, green: 42, blue: 44 }
-function rgbTint(rgb, i) {
+function rgbTint(rgb, i, mode) {
+  var percent = mode === "normal" ? 0.2 : 0.1;
+
   return {
-    red: rgb.red + (255 - rgb.red) * i * 0.1,
-    green: rgb.green + (255 - rgb.green) * i * 0.1,
-    blue: rgb.blue + (255 - rgb.blue) * i * 0.1
-  }
+    red: rgb.red + (255 - rgb.red) * i * percent,
+    green: rgb.green + (255 - rgb.green) * i * percent,
+    blue: rgb.blue + (255 - rgb.blue) * i * percent,
+  };
 }
 
 // take a hex color string and produce a list of 10 tints or shades of that color
 // shadeOrTint should be either `rgbShade` or `rgbTint`, as defined above
 // this allows us to use `calculate` for both shade and tint
-function calculate(colorValue, shadeOrTint) {
+function calculate(colorValue, shadeOrTint, mode) {
   var color = hexToRGB(colorValue);
   var shadeValues = [];
-
-  for (var i = 0; i < 10; i++) {
-    shadeValues[i] = rgbToHex(shadeOrTint(color, i));
+  var shadeValuesLength = mode === "normal" ? 5 : 9;
+  for (var i = 1; i < shadeValuesLength; i++) {
+    shadeValues[i - 1] = rgbToHex(shadeOrTint(color, i, mode));
   }
   return shadeValues;
 }
 
 // given a color value, return an array of ten shades in 10% increments
-function calculateShades(colorValue) {
-  return calculate(colorValue, rgbShade).concat("000000");
+function calculateShades(colorValue, mode) {
+  return calculate(colorValue, rgbShade, mode);
 }
 
 // given a color value, return an array of ten tints in 10% increments
-function calculateTints(colorValue) {
-  return calculate(colorValue, rgbTint).concat("ffffff");
+function calculateTints(colorValue, mode) {
+  return calculate(colorValue, rgbTint, mode);
 }
 
 function updateClipboardData() {
@@ -124,12 +126,21 @@ function makeTableRowColors(colors, displayType) {
   var tableRow = "<tr>";
   for (var i = 0; i < colors.length; i++) {
     var color = colors[i].toString(16);
-    if (displayType == "colors") { // make a row of colors
+    if (displayType == "colors") {
+      // make a row of colors
       // we have to account for the prefix here in case the user toggled the checkbox before generating another palette
       var colorPrefix = settings.copyWithHashtag ? "#" : "";
-      tableRow += "<td tabindex=\"0\" role=\"button\" aria-label=\"Color swatch\" class=\"hex-color\" style=\"background-color:" + "#" + color + "\" data-clipboard-text=\"" + colorPrefix + color + "\"></td>";
-    } else { // make a row of RGB values
-      tableRow += "<td class=\"hex-value\">" + color.toUpperCase() + "</td>";
+      tableRow +=
+        '<td tabindex="0" role="button" aria-label="Color swatch" class="hex-color" style="background-color:' +
+        "#" +
+        color +
+        '" data-clipboard-text="' +
+        colorPrefix +
+        color +
+        '"></td>';
+    } else {
+      // make a row of RGB values
+      tableRow += '<td class="hex-value">' + color.toUpperCase() + "</td>";
     }
   }
   tableRow += "</tr>";
@@ -141,38 +152,66 @@ function createTintsAndShades(firstTime) {
   if (parsedColorsArray !== null) {
     // make sure we got value color values back from parsing
     var colorDisplayRows = []; // holds html table rows for the colors to display
-    var tableRowCounter = 0;
+    var extraColorDisplayRows = []; // holds html table rows for the colors to display
 
-    for (var i = 0; i < parsedColorsArray.length; i++) { // iterate through each inputted color value
+    var tableRowCounter = 0;
+    var extraTableRowCounter = 0;
+
+    for (var i = 0; i < parsedColorsArray.length; i++) {
+      // iterate through each inputted color value
 
       // calculate an array of shades from the inputted color, then make a table row
       // from the shades, and a second table row for the hex values of the shades
-      var calculatedShades = calculateShades(parsedColorsArray[i]);
-      colorDisplayRows[tableRowCounter] = makeTableRowColors(calculatedShades, "colors");
-      tableRowCounter++;
-      colorDisplayRows[tableRowCounter] = makeTableRowColors(calculatedShades, "RGBValues");
-      tableRowCounter++;
-
+      var calculatedShades = calculateShades(parsedColorsArray[i], "normal");
       // calculate an array of tints from the inputted color, then make a table row
       // from the tints, and a second table row for the hex values of the tints
-      var calculatedTints = calculateTints(parsedColorsArray[i]);
-      colorDisplayRows[tableRowCounter] = makeTableRowColors(calculatedTints, "colors");
+      var calculatedTints = calculateTints(parsedColorsArray[i], "normal")
+        .reverse()
+        .concat(parsedColorsArray[i]);
+      var calculatedShadesAndTints = calculatedTints.concat(calculatedShades);
+
+      colorDisplayRows[tableRowCounter] = makeTableRowColors(calculatedShadesAndTints, "colors");
       tableRowCounter++;
-      colorDisplayRows[tableRowCounter] = makeTableRowColors(calculatedTints, "RGBValues");
+      colorDisplayRows[tableRowCounter] = makeTableRowColors(calculatedShadesAndTints, "RGBValues");
       tableRowCounter++;
+
+      // extra mode
+      var calculatedExtraShades = calculateShades(parsedColorsArray[i], "extra");
+      var calculatedExtraTints = calculateTints(parsedColorsArray[i], "extra")
+        .reverse()
+        .concat(parsedColorsArray[i]);
+      var calculatedExtraShadesAndTints = calculatedExtraTints.concat(calculatedExtraShades);
+
+      extraColorDisplayRows[extraTableRowCounter] = makeTableRowColors(
+        calculatedExtraShadesAndTints,
+        "colors",
+      );
+      extraTableRowCounter++;
+      extraColorDisplayRows[extraTableRowCounter] = makeTableRowColors(
+        calculatedExtraShadesAndTints,
+        "RGBValues",
+      );
+      extraTableRowCounter++;
     }
 
     // wrap the rows into an HTML table with a hard-coded header row
-    var colorDisplayTable = "<table><thead><tr class=\"table-header\"><td><span>0%</span></td><td><span>10%</span></td><td><span>20%</span></td><td><span>30%</span></td><td><span>40%</span></td><td><span>50%</span></td><td><span>60%</span></td><td><span>70%</span></td><td><span>80%</span></td><td><span>90%</span></td><td><span>100%</span></td></tr></thead>" + colorDisplayRows.join("") + "</table>";
-
+    var colorDisplayNormalTable =
+      '<table><thead><tr class="table-header"><td><span>100</span></td><td><span>200</span></td><td><span>300</span></td><td><span>400</span></td><td><span>500</span></td><td><span>600</span></td><td><span>700</span></td><td><span>800</span></td><td><span>900</span></td></tr></thead>' +
+      colorDisplayRows.join("") +
+      "</table>";
+    var colorDisplayExtraTable =
+      '<table><thead><tr class="table-header"><td><span>100</span></td><td><span>150</span></td><td><span>200</span></td><td><span>250</span></td><td><span>300</span></td><td><span>350</span></td><td><span>400</span></td><td><span>450</span></td><td><span>500</span></td><td><span>550</span></td><td><span>600</span></td><td><span>650</span></td><td><span>700</span></td><td><span>750</span></td><td><span>800</span></td><td><span>850</span></td><td><span>900</span></td></thead>' +
+      extraColorDisplayRows.join("") +
+      "</table>";
     // replace tints-and-shades div with color display table wrapped by the same div
-    $("#tints-and-shades").html(colorDisplayTable);
+    $("#normal #tints-and-shades").html(colorDisplayNormalTable);
+    $("#extra #tints-and-shades").html(colorDisplayExtraTable);
 
     // set url hash to a comma seperated list of hex codes
     window.location.hash = parsedColorsArray.join(",");
 
     // scroll down to show the tints-and-shades div
-    $('html,body').animate({ scrollTop: $("#ts-scroll-top").offset().top }, 400);
+    $("html,body").animate({ scrollTop: $("#ts-scroll-top").offset().top }, 400);
 
     // set focus to the color display table after 400 milliseconds
     setTimeout(function () {
@@ -183,24 +222,27 @@ function createTintsAndShades(firstTime) {
     // when color display table loses focus, make it not focusable again
     $("#tints-and-shades").blur(function () {
       $("#tints-and-shades").attr("tabindex", "-1");
-    })
-  } else if (firstTime != true) { // doesn't run on page load (the first time it runs)
-    // scroll back to top of page
-    $('html,body').stop().animate({ scrollTop: 0 }, 200, function () {
-      // remove any existing content from tints-and-shades div
-      $("#tints-and-shades").html("");
-
-      // reset the url hash
-      window.location.hash = "";
-
-      // show warning
-      $("#warning").addClass("visible");
-
-      // hide warning after 3 seconds
-      setTimeout(function () {
-        $("#warning").removeClass("visible");
-      }, 3000);
     });
+  } else if (firstTime != true) {
+    // doesn't run on page load (the first time it runs)
+    // scroll back to top of page
+    $("html,body")
+      .stop()
+      .animate({ scrollTop: 0 }, 200, function () {
+        // remove any existing content from tints-and-shades div
+        $("#tints-and-shades").html("");
+
+        // reset the url hash
+        window.location.hash = "";
+
+        // show warning
+        $("#warning").addClass("visible");
+
+        // hide warning after 3 seconds
+        setTimeout(function () {
+          $("#warning").removeClass("visible");
+        }, 3000);
+      });
 
     // set focus back to the text area
     $("#color-values").focus();
@@ -229,14 +271,13 @@ $(document).ready(function () {
 
 // Checks if the enter key is pressed and simulates a click on the focused element
 $(document).keypress(function (event) {
-  if ((event.keyCode ? event.keyCode : event.which) == "13")
-    $(":focus").click();
+  if ((event.keyCode ? event.keyCode : event.which) == "13") $(":focus").click();
 });
 
 // Show a new Carbon ad every time certain elements are clicked
-$(document).on('click', '#make', function () {
+$(document).on("click", "#make", function () {
   // If the ad hasn't loaded yet, don't refresh it while it's still loading, or it will return two (or more) ads
   if (!$("#carbonads")[0]) return;
   // If the script hasn't loaded, don't try calling it
-  if (typeof _carbonads !== 'undefined') _carbonads.refresh();
+  if (typeof _carbonads !== "undefined") _carbonads.refresh();
 });
